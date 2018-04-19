f5e47ef069711f6b780611290dee1ab0ebc98a2c
mrinal.k <mrinal.k@endurance.com>
2018-04-19 21:15:42 +0530

    Fix get post for upcomingevents

--- AIW/upcomingevents/views.py
+++ AIW/upcomingevents/views.py
@@ -4,17 +4,21 @@ from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from upcomingevents.models import Event
+from upcomingevents.utils import *
 
 class UpcomingEvents(TemplateView):
     template_name = 'features.html'
 
     def get(self, request, *args, **kwargs):
-        events = Event.objects.order_by('date')
+        user_email_address = request.session['email_address']
+        events = get_unregistered_events(user_email_address)        
         print(events)
         context = {'events': events}
         return render(request, self.template_name, context=context)
-
-
+    def post(self, request, *args, **kwargs):
+        user_email_address = request.session['email_address']
+        register_event((request.POST.get("event_id", "")),user_email_address,0)        
+        return HttpResponseRedirect("/homepage")
 
 
 
