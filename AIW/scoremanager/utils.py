6545026f2f4fc12bdb72c4d8635b6553dd418bc4
anusha.sr <anusha.sr@endurance.com>
2018-04-19 21:37:01 +0530

    Added get for scoreboard

--- AIW/scoremanager/utils.py
+++ AIW/scoremanager/utils.py
@@ -1,9 +1,9 @@
 from upcomingevents.models import Event
 from homepagemanager.models import RegisteredEvents
+from signupmanager.models import UserData
 
 def user_events_details(user_email_address):
     events_registered = RegisteredEvents.objects.filter(volunteer_email = user_email_address)
-    print(events_registered)
     events_participated = []
     total_score=0
     score_received_per_event=[]
@@ -14,7 +14,6 @@ def user_events_details(user_email_address):
             event_details = Event.objects.filter(id = event.event_id)
             for subevent in event_details:
                 events_participated.append(subevent)
-    print(events_participated)
     events_hosted = Event.objects.filter(host_email = user_email_address)
     volunteers_per_event=[]
     for event in events_hosted:
@@ -22,8 +21,27 @@ def user_events_details(user_email_address):
          volunteer_rows = RegisteredEvents.objects.filter(event_id = event.id)
          for volunteer in volunteer_rows:
              volunteers.append(volunteer.volunteer_email)
-         print(volunteers)
          volunteers_per_event.append(volunteers)
-    print(volunteers_per_event)
     return events_participated,events_hosted,total_score,volunteers_per_event,score_received_per_event
 
+
+def get_events_participated(email_address):
+    registered_events = RegisteredEvents.objects.filter(volunteer_email = email_address)
+    participated_events=[]
+    total_score = 0
+    for event in registered_events:
+        row = Event.objects.filter(id=event.id)[0]
+        participated_events.append({'event_name':row.event_name, 'description':row.description, 'date':row.date, 'location':row.location, 'score':event.score})
+        total_score+=event.score
+    return participated_events, total_score
+
+def get_events_hosted(email_address):
+    hosted_events = Event.objects.filter(host_email = email_address)
+    events = []
+    for event in hosted_events:
+        rows = RegisteredEvents.objects.filter(event_id = event.id)
+        volunteers = []
+        for row in rows:
+            volunteers.append(UserData.objects.filter(email_address = row.volunteer_email)[0].full_name)
+        events.append({'event_name':event.event_name, 'description':event.description, 'date':event.date, 'location':event.location, 'volunteers':volunteers})
+    return events
--- AIW/scoremanager/views.py
+++ AIW/scoremanager/views.py
@@ -4,19 +4,18 @@ from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from upcomingevents.models import Event
 from homepagemanager.models import RegisteredEvents
-from scoremanager.utils import *
+from scoremanager.utils import get_events_participated, get_events_hosted
 
 class ScoreBoardView(TemplateView):
     template_name = 'scoreboard.html'
 
     def get(self, request, *args, **kwargs):
-        user_email_address = request.session['email_address']
-        events_participated,events_hosted,total_score,volunteers_per_event,score_received_per_event=user_events_details(user_email_address)
+        email_address = request.session['email_address']
+        events_participated, total_score = get_events_participated(email_address)
+        hosted_events = get_events_hosted(email_address)
         context = {
-            'events_registered' : events_participated,
-            'events_hosted' : events_hosted,
+            'events_participated' : events_participated,
             'total_score' : total_score,
-            'volunteers_per_event' : volunteers_per_event,
-            'score_received_per_event' : score_received_per_event
+            'hosted_events' : hosted_events
         }
         return render(request, self.template_name, context=context)
