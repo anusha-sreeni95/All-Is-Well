77d6af36f5185f74aaab1751948de9ed5e4ca7a2
anusha.sr <anusha.sr@endurance.com>
2018-04-19 19:42:21 +0530

    Minor corrections to homepage get function

--- AIW/homepagemanager/utils.py
+++ AIW/homepagemanager/utils.py
@@ -3,9 +3,5 @@ from homepagemanager.models import RegisteredEvents
 
 def user_events_details(user_email_address):
     events_registered = RegisteredEvents.objects.filter(volunteer_email = user_email_address)
-    print(events_registered)
-    
     events_hosted = Event.objects.filter(host_email = user_email_address)
-    
     return events_registered,events_hosted
-	
--- AIW/homepagemanager/views.py
+++ AIW/homepagemanager/views.py
@@ -7,10 +7,9 @@ class HomePageView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         user_email_address = request.session['email_address']
-        events_registered,events_hosted=user_events_details(user_email_address)
+        events_registered, events_hosted = user_events_details(user_email_address)
         context = {
             'events_registered' : events_registered,
             'events_hosted' : events_hosted,
-            
         }
-        return render(request, self.template_name)
+        return render(request, self.template_name, context=context)
