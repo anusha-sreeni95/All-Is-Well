3ef473820a8ef8dd18b78f775cd840f820f27d19
anusha.sr <anusha.sr@endurance.com>
2018-04-20 11:34:34 +0530

    Fixed scoreboard functionality

--- AIW/scoremanager/utils.py
+++ AIW/scoremanager/utils.py
@@ -7,7 +7,7 @@ def get_events_participated(email_address):
     participated_events=[]
     total_score = 0
     for event in registered_events:
-        rows = Event.objects.filter(id=event.id)
+        rows = Event.objects.filter(id=event.event_id)
         for row in rows:
             participated_events.append({'event_name':row.event_name, 'description':row.description, 'date':row.date, 'location':row.location, 'score':event.score})
         total_score+=event.score
@@ -15,14 +15,18 @@ def get_events_participated(email_address):
 
 def get_events_hosted(email_address):
     hosted_events = Event.objects.filter(host_email = email_address)
-    events = []
     for event in hosted_events:
-        rows = RegisteredEvents.objects.filter(event_id = event.id)
+        events = []
+        registered_participants = RegisteredEvents.objects.filter(event_id = event.id)
         volunteers = []
-        for row in rows:
-            users = UserData.objects.filter(email_address = row.volunteer_email)
-            for user in users:
-                volunteers.append({'email_address':row.volunteer_email, 'name':user.full_name})
+        for registered_participant in registered_participants:
+            if(registered_participant.score!=0):
+                not_scored = False
+            else:
+                not_scored = True
+            user_details = UserData.objects.filter(email_address = registered_participant.volunteer_email)
+            for user in user_details:
+                volunteers.append({'email_address':user.email_address, 'name':user.full_name, 'not_scored': not_scored})
         events.append({'id':event.id, 'event_name':event.event_name, 'description':event.description, 'date':event.date, 'location':event.location, 'volunteers':volunteers})
     return events
 
