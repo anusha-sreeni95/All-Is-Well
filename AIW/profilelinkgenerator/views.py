064bc26f0c7fd859f9dae604e92d0f55abd86e56
anusha.sr <anusha.sr@endurance.com>
2018-04-20 09:41:36 +0530

    Listed events in profile view

--- AIW/profilelinkgenerator/views.py
+++ AIW/profilelinkgenerator/views.py
@@ -3,17 +3,23 @@ from django.views.generic import TemplateView
 from django.http import HttpResponse, HttpResponseRedirect
 from .utils import get_email_address
 from signupmanager.models import UserData
+from scoremanager.utils import get_events_participated, get_events_hosted
 
 class ProfilePage(TemplateView):
     template_name = 'profilepage.html'
 
     def get(self, request, token):
-        print(token)
         email_address = get_email_address(token)
         if(email_address!=None):
             profile = UserData.objects.filter(email_address=email_address)[0]
+            events_participated, total_score = get_events_participated(email_address)
+            hosted_events = get_events_hosted(email_address)
+            print(hosted_events)
             context = {
-                'profile':profile
+                'profile':profile,
+                'events_participated' : events_participated,
+                'hosted_events' : hosted_events,
+                'total_score' : total_score
             }
             return render(request, self.template_name, context=context)
         else:
